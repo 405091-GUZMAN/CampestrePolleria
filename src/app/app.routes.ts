c3d5670b78ece1589d01c7b50a71bce709537394
405091-GUZMAN <405091@tecnicatura.frc.utn.edu.ar>
2025-07-17 19:58:48 -0300

    ajustes para smartphone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import {RouterModule, Routes} from '@angular/router';
 import {LandingPage} from './components/landing-page/landing-page';
 import {ProductCatalog} from './components/product-catalog/product-catalog';
 import {AboutUs} from './components/about-us/about-us';
-import {ShoppingCart} from './components/shopping-cart/shopping-cart';
 import {Contact} from './components/contact/contact';
 import {NgModule} from '@angular/core';
 
@@ -10,7 +9,6 @@ export const routes: Routes = [
   { path: 'nosotros', component: AboutUs },
   { path: 'pedido', component: ProductCatalog },
   { path: 'contacto', component: Contact },
-  { path: 'carrito', component: ShoppingCart },
   { path: '', redirectTo: 'nosotros', pathMatch: 'full' }, // Redirección inicial
   { path: '**', redirectTo: 'nosotros' } // Ruta comodín
 ];
